4190f78ca274768398f58231ecd2091f3bf7070c
Autumn Williams <63067844+AutumnHW@users.noreply.github.com>
2026-02-14 00:06:44 -0500

    added a command clear script + config.json template file

---
+++ cleanup-commands.js
@@ -0,0 +1,22 @@
+//Run this .js file in your terminal to unregister slash commands...
+//useful for removing endpoints that will never respond in the case of test bot applications.
+
+const { REST, Routes } = require('discord.js');
+const { clientId, guildId, token } = require('./config.json');
+
+const rest = new REST().setToken(token);
+
+(async () => {
+  try {
+    console.log('Started clearing application (/) commands...');
+
+    await rest.put(
+      Routes.applicationGuildCommands(clientId, guildId),
+      { body: [] },
+    );
+
+    console.log('Successfully cleared all guild slash commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
