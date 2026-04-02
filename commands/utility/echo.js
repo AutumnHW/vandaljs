9e1c828ce2c17d3324bf9259ad94ba258db55ac4
Autumn Williams <63067844+AutumnHW@users.noreply.github.com>
2026-04-02 12:53:54 -0400

    fixed echo

--- commands/utility/echo.js
+++ commands/utility/echo.js
@@ -1,25 +1,34 @@
-const { SlashCommandBuilder } = require('discord.js');
-const userData = require(process.cwd() + '/modules/userdata.js');
+const {
+    ContextMenuCommandBuilder,
+    ApplicationCommandType,
+    ActionRowBuilder,
+    ChannelSelectMenuBuilder,
+    ChannelType
+} = require('discord.js');
+
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('echo')
-        .setDescription('Echos a supplied message to a specified channel')
-        .addStringOption((option) => option.setName('input').setDescription('The message you want to send').setRequired(true))
-	    .addChannelOption((option) => option.setName('channel').setDescription('The channel you want to send your message to').setRequired(true)),
+    data: new ContextMenuCommandBuilder()
+        .setName('Echo')
+        .setType(ApplicationCommandType.Message),
 
     async execute(interaction) {
-        const message = interaction.options.getString('input');
-        const channel = interaction.options.getChannel('channel');
-        //const username = target.username;
-        if (!userData.authenticateUser(interaction.member.id)) { };
-        //console.log(userData.getUserInfo(interaction.user, 'permissions'));
-        //console.log('target: ' + target + 'qty: ' + qty);
-        if (!userData.authenticateUser(interaction.member.id)) {
-            await interaction.reply('You do not have permission to execute this command.');
+        const targetMessage = interaction.targetMessage;
 
-        } else {
-            channel.send(message);
-            await interaction.reply('Ok!  Message has been sent.');
-        };
-    },
+        const select = new ChannelSelectMenuBuilder()
+            .setCustomId(`echo_select_${interaction.id}`)
+            .setPlaceholder('Select a channel')
+            .addChannelTypes(ChannelType.GuildText); // restrict if you want
+
+        const row = new ActionRowBuilder().addComponents(select);
+
+        await interaction.reply({
+            content: 'Where do you want to send this message?',
+            components: [row],
+            ephemeral: true
+        });
+
+        // Store message temporarily
+        interaction.client.echoCache ??= new Map();
+        interaction.client.echoCache.set(interaction.id, targetMessage);
+    }
 };
--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,24 +1,75 @@
 const { Events, MessageFlags } = require('discord.js');
 
 module.exports = {
-	name: Events.InteractionCreate,
-	async execute(interaction) {
-		if (!interaction.isChatInputCommand()) return;
-		const command = interaction.client.commands.get(interaction.commandName);
-		if (!command) {
-			console.error(`No command matching ${interaction.commandName} was found.`);
-			return;
-		}
+    name: Events.InteractionCreate,
+    async execute(interaction) {
+        if (interaction.isChannelSelectMenu()) {
+            const id = interaction.customId.split('echo_select_')[1];
+            const message = interaction.client.echoCache?.get(id);
 
-		try {
-			await command.execute(interaction);
-		} catch (error) {
-			console.error(error);
-			if (interaction.replied || interaction.deferred) {
-				await interaction.followUp({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
-			} else {
-				await interaction.reply({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
-			}
-		}
-	},
+            if (!message) {
+                return interaction.reply({
+                    content: 'Original message not found.',
+                    ephemeral: true
+                });
+            }
+
+            const channel = interaction.channels.first();
+
+            try {
+                await channel.send({
+                    content: message.content,
+                    embeds: message.embeds,
+                    files: message.attachments.map(a => a.url)
+                });
+
+                await interaction.update({
+                    content: `Sent to ${channel}`,
+                    components: []
+                });
+
+                interaction.client.echoCache.delete(id);
+
+            } catch (err) {
+                console.error(err);
+
+                return interaction.reply({
+                    content: 'Failed to send message.',
+                    ephemeral: true
+                });
+            }
+
+            return;
+        }
+
+  
+        if (
+            !interaction.isChatInputCommand() &&
+            !interaction.isMessageContextMenuCommand()
+        ) return;
+
+        const command = interaction.client.commands.get(interaction.commandName);
+
+        if (!command) {
+            console.error(`No command matching ${interaction.commandName} was found.`);
+            return;
+        }
+
+        try {
+            await command.execute(interaction);
+        } catch (error) {
+            console.error(error);
+
+            const replyPayload = {
+                content: 'There was an error while executing this command!',
+                flags: MessageFlags.Ephemeral
+            };
+
+            if (interaction.replied || interaction.deferred) {
+                await interaction.followUp(replyPayload);
+            } else {
+                await interaction.reply(replyPayload);
+            }
+        }
+    },
 };
