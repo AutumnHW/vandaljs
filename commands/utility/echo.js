4c7fcbf79d961935275031caf20329b9d2c5dfdb
Autumn Williams <63067844+AutumnHW@users.noreply.github.com>
2026-04-02 12:58:37 -0400

    added permissions checking on the echo command

--- commands/utility/echo.js
+++ commands/utility/echo.js
@@ -5,7 +5,7 @@ const {
     ChannelSelectMenuBuilder,
     ChannelType
 } = require('discord.js');
-
+const userData = require(process.cwd() + '/modules/userdata.js')
 module.exports = {
     data: new ContextMenuCommandBuilder()
         .setName('Echo')
@@ -13,22 +13,25 @@ module.exports = {
 
     async execute(interaction) {
         const targetMessage = interaction.targetMessage;
+   if (!userData.authenticateUser(interaction.member.id)) {
+            await interaction.reply('You do not have permission to execute this command.');
 
+        } else {
         const select = new ChannelSelectMenuBuilder()
             .setCustomId(`echo_select_${interaction.id}`)
             .setPlaceholder('Select a channel')
             .addChannelTypes(ChannelType.GuildText); // restrict if you want
-
         const row = new ActionRowBuilder().addComponents(select);
-
         await interaction.reply({
             content: 'Where do you want to send this message?',
             components: [row],
             ephemeral: true
         });
-
         // Store message temporarily
         interaction.client.echoCache ??= new Map();
         interaction.client.echoCache.set(interaction.id, targetMessage);
+        };
+
+   
     }
 };
